66969e8f98424dcaa8c0de656c76c588017f4d3f
supera-wear <ramazan.barlas@gmail.com>
2025-06-11 13:37:01 +0200

    Fix nodemailer createTransporter error

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const users = [];
 const emailVerificationTokens = new Map();
 
 // Email transporter configuration
-const emailTransporter = nodemailer.createTransporter({
+const emailTransporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST || 'smtp.gmail.com',
     port: process.env.EMAIL_PORT || 587,
     secure: false,
